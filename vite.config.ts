8fd5d506765cfbf776377c3afdc6433f32a50a44
Valdo <vteixeiraromao@gmail.com>
2022-07-15 11:15:44 +0100

    Updated repo + added POAP logos

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { getMapsOptimizers, getMapsScripts } from "wa-map-optimizer-vite";
 
 export default defineConfig({
+    base: "./",
     build: {
         rollupOptions: {
             input: {
@@ -17,6 +18,7 @@ export default defineConfig({
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
             "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+            "Cache-Control": "no-cache, no-store, must-revalidate",
         },
         open: "/",
     },
